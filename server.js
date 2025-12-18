095e35d77818e81c24340286bc9b981772c53748
Eni Fred <enikfred@gmail.com>
2025-12-18 15:32:19 +0100

    changed fronend url

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend-url.vercel.app",
+      "https://cbt-lime.vercel.app",
     ],
     credentials: true,
   })

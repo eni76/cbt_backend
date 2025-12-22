63177509bf261e20be3387e6270d49a71972cd57
Eni Fred <enikfred@gmail.com>
2025-12-22 17:27:48 +0100

    revised register3

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -3,4 +3,5 @@ import { registerSchool } from "../controllers/userController.js";
 import uploads from "../middleware/uploads.js";
 const userRouter = express.Router();
 userRouter.post("/register", uploads.single("image"), registerSchool);
+userRouter.post("/login", registerSchool);
 export { userRouter };
